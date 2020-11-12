87371dafe1d319c8a2837c6a7397f506903e8fd7
vitor.marques <vitor.marques@gatec.com.br>
2020-11-12 20:51:29 -0300

    Started to implement NumberFormatter

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import controllers.Controller;
+
 public class Main {
 
-  public static void main(String args[]) {
-    System.out.println("Hello, World!");
-  }
+	public static void main(String args[]) {
+		
+		Controller c = new Controller();
+		
+		double a = 12450900;
+		String result = c.formatDouble(a);
+		
+		System.out.println(result);
+	}
 }
--- src/controllers/Controller.java
+++ src/controllers/Controller.java
@@ -1,7 +1,15 @@
 package controllers;
 
+import models.NumberFormatter;
+
 public class Controller {
-  public void hello() {
-    System.out.println("Hello");
-  }
+	public void hello() {
+		System.out.println("Hello");
+	}
+
+	public String formatDouble(double number) {
+		NumberFormatter formatter = new NumberFormatter();
+		
+		return formatter.formatDouble(number);
+	}
 }
