28c428925d6825cd63d263bedec161173e03f24f
gabrielsanttana <gabriel.gsantana7@gmail.com>
2020-11-12 22:39:30 -0300

    Create initial Converter view with labels, input and selects

--- src/views/Converter.java
+++ src/views/Converter.java
@@ -6,30 +6,61 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class Converter implements ActionListener {
 
   private JFrame frame;
   private JPanel panel;
+  private JPanel convertFromPanel;
+  private JPanel toPanel;
   private JLabel label;
+  private JLabel fromLabel;
+  private JLabel toLabel;
   private JButton button;
+  private JTextField fromInput;
+  private JTextField toInput;
+  private JComboBox fromSelect;
+  private JComboBox toSelect;
   private int count;
 
   public Converter() {
     frame = new JFrame();
 
-    button = new JButton("Click me");
-    button.addActionListener(this);
-    label = new JLabel("Number of clicks: 0");
+    convertFromPanel = new JPanel();
+    toPanel = new JPanel();
+    
+    fromLabel = new JLabel("Convert from");
+    toLabel = new JLabel("to");
+
+    fromInput = new JTextField();
+    toInput = new JTextField();
+
+    fromSelect = new JComboBox();
+    toSelect = new JComboBox();
+
+    fromSelect.addItem("Unidade 1");
+    fromSelect.addItem("Unidade 2");
+    fromSelect.addItem("Unidade 3");
+
+    toSelect.addItem("Unidade 1");
+    toSelect.addItem("Unidade 2");
+    toSelect.addItem("Unidade 3");
 
     panel = new JPanel();
     panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
     panel.setLayout(new GridLayout(0, 1));
-    panel.add(button);
-    panel.add(label);
+    
+    panel.add(fromLabel);
+    panel.add(fromInput);
+    panel.add(fromSelect);
+    panel.add(toLabel);
+    panel.add(toSelect);
+    panel.add(toInput);
 
     frame.add(panel, BorderLayout.CENTER);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
