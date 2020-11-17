1790e4a3c5d80e4c70618d58059da93df8e8de85
gabrielsanttana <gabriel.gsantana7@gmail.com>
2020-11-17 19:50:17 -0300

    wip

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
+import java.util.List;
+import controllers.Controller;
+import models.IConverter;
+
 public class Main {
 
-  public static void main(String args[]) {
-    System.out.println("Hello, World!");
+  public static void main(String[] args) {
+    Controller controller = new Controller();
+    
+    List<Class<IConverter>> classes = null;
+    try {
+      classes = controller.lookForConverterClasses();
+    } catch (Exception e) {
+    }
+    
+    if (classes != null) {
+      for (Class<IConverter> cls : classes) {
+        System.out.println(cls.getName());
+      }
+    }
   }
 }
