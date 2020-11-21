7625d526b1582872dec026ecd9a81111266ca627
gabrielsanttana <gabriel.gsantana7@gmail.com>
2020-11-21 19:07:37 -0300

    Add MillimetreConverter

---
+++ src/converters/MillimetreConveter.java
@@ -0,0 +1,20 @@
+package converters;
+
+import models.MeasureType;
+
+public class MillimetreConveter extends AbstractConverter {
+
+  public MillimetreConveter(MeasureType type) {
+    super(type);
+  }
+
+  @Override
+  public double fromBasicUnit(double baseUnit) {
+    return baseUnit / 1000;
+  }
+
+  @Override
+  public double toBasicUnit(double value) {
+    return value * 1000;
+  }
+}
