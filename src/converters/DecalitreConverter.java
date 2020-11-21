e43b5e28572bc36d93fc2d41b7b8dc8aa18bf1a8
gabrielsanttana <gabriel.gsantana7@gmail.com>
2020-11-21 19:14:00 -0300

    Add DecalitreConverter

--- src/converters/DecalitreConverter.java
+++ src/converters/DecalitreConverter.java
@@ -2,19 +2,19 @@ package converters;
 
 import models.MeasureType;
 
-public class InchConverter extends AbstractConverter {
+public class DecalitreConverter extends AbstractConverter {
 
-  public InchConverter(MeasureType type) {
+  public DecalitreConverter(MeasureType type) {
     super(type);
   }
 
   @Override
   public double fromBasicUnit(double baseUnit) {
-    return baseUnit * 39.3701;
+    return baseUnit / 10;
   }
 
   @Override
   public double toBasicUnit(double value) {
-    return value * 0.0254;
+    return value * 10;
   }
 }
